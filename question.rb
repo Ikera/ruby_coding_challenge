c28c9b12b1c025b7d13ffecfa66b1cbf6a103fa1
Ivica Lakatos <ivica@orangeiceberg.com>
2019-12-06 08:05:05 +0100

    Implement a solution

--- question.rb
+++ question.rb
@@ -4,6 +4,8 @@
 # Example -- [2, 1, 3, 4] #=> [12, 24, 8, 6]
 class Question
   class << self
-    def solution(arr); end
+    def solution(arr)
+      arr.map.with_index { |_, idx| arr.reject.with_index { |el, i| i == idx }.inject(:*) }
+    end
   end
 end
